3cba0a0cc2837b55d8535b3d494abda466ead2c8
David-kun <thequen999@gmail.com>
2025-11-19 21:17:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import pandas as pd
+dummy = ''
 flag = True
+DF = pd.read_csv("data_pengguna.csv")
 def registrasi():
     while True:
         nama = input("Masukkan nama anda: ")
@@ -25,31 +28,94 @@ def registrasi():
         else:
             break
     return nama,umur,hobi,kota
-print('=== === === form registrasi === === ===')
-nama, umur, hobi, kota = registrasi()
-while flag == True:
-    Auth = input(f'''apakah data ini sudah benar?
-    nama\t: {nama}
-    umur\t: {umur}
-    hobi\t: {hobi}
-    kota\t: {kota}
-[Y]/[N]?''')
-    Auth = Auth.lower()
-    if Auth == 'n':
-        print('silahkan masukan ulang data anda')
-        registrasi()
-    elif Auth == 'y':
-        print('data registrasi disimpan')
+def dataPengguna():
+    print("=== === === Daftar Nama Pengguna === === ===")
+    DF = pd.read_csv("data_pengguna.csv")
+    for i,nama in enumerate(DF['nama']):
+        print(f"{i:04d} {nama}")
+        dummy = input()
+
+def register():
+    flag = True
+    print('=== === === form registrasi === === ===')
+    nama, umur, hobi, kota = registrasi()
+    while flag == True:
+        Auth = input(f'''apakah data ini sudah benar?
+Nama\t: {nama}
+Umur\t: {umur}
+Hobi\t: {hobi}
+Kota\t: {kota}
+[Y]/[N]? >>> ''')
+        Auth = Auth.lower()
+        if Auth == 'n':
+            print('silahkan masukan ulang data anda')
+            nama, umur, hobi, kota = registrasi()
+        elif Auth == 'y':
+            nama, umur, hobi, kota
+            with open("data_pengguna.csv", "a+") as file:
+                file.write(f"{nama}, {umur}, {hobi}, {kota}\n")            
+            print('data registrasi disimpan')
+            break
+        else:
+            while True:
+                Auth = input('silahkan masukkan Y/N saja!!')
+                Auth = Auth.lower()
+                if Auth == 'n':
+                    print('silahkan masukan ulang data anda')
+                    nama, umur, hobi, kota = registrasi()
+                    break
+                elif Auth == 'y':
+                    print('data registrasi disimpan')
+                    nama, umur, hobi, kota
+                    with open("data_pengguna.csv", "a+") as file:
+                        file.write(f"{nama}, {umur}, {hobi}, {kota}\n")            
+                    print('data registrasi disimpan')
+                    flag = False
+                    dummy = input()
+                    break
+def lihatPengguna():
+    while True:
+        id_str = input("Masukkan ID (4 digit) >>> ")
+
+        if len(id_str) != 4 or not id_str.isdigit():
+            print("ID tidak valid\n")
+            continue
+
+        id_user = int(id_str)
+
+        if id_user >= len(DF):
+            print("ID tidak ditemukan\n")
+            continue
+        elif id_user < 0 or id_user >= len(DF):
+            print("ID tidak ditemukan")
+            continue
         break
-    else:
-        while True:
-            Auth = input('silahkan masukkan Y/N saja!!')
-            Auth = Auth.lower()
-            if Auth == 'n':
-                print('silahkan masukan ulang data anda')
-                registrasi()
-                break
-            elif Auth == 'y':
-                print('data registrasi disimpan')
-                flag = False
-                break
+
+    row = DF.loc[id_user]
+
+    print(f"""\n=== === === Data pengguna {id_user:04d} === === ===
+Nama    : {row['nama']}
+Umur    : {row['umur']}
+Hobi    : {row['hobi']}
+Kota    : {row['kota']}""")
+    dummy = input()
+
+
+def main():
+    while True:
+        try:
+            pilih = int(input('''=== === === Menu Aplikasi === === ===
+1. lihat data semua pengguna
+2. masukkan data pengguna baru
+3. lihat data pengguna
+>>> '''))
+        except ValueError:
+            print('harap masukkan angka')
+            continue
+        if pilih == 1:
+            dataPengguna()
+        elif pilih == 2:
+            register()
+        elif pilih == 3:
+            lihatPengguna()
+main()
